7d06155a174a5494fd56b0c99588c0356843ab29
nibble-nb <74096501+nibble-nb@users.noreply.github.com>
2020-11-08 11:31:55 +0530

    coded  tut-1.java

---
+++ tut-1.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World! \n");
+    // statement ends with ;
+    // but some statements dont end we would find about it in future 
+    // print your name and comment code in comments below 
+    System.out.println(" NIBBLE "); 
+  }
+}
+// NIBBLE
